0924f3e1584e23eeb067bc1f4509cfaec82c6faf
Mittesh <122700425+Mittesh-9@users.noreply.github.com>
2023-07-15 00:39:52 +0530

    Add files via upload

---
+++ check_website_connectivity.py
@@ -0,0 +1,20 @@
+import csv
+import requests
+
+status_dict = {"Website":"Status"}
+
+
+def main():
+    with open(r"C:\Users\Mittesh Waghule\Downloads\websites.txt", "r") as fr:
+        for line in fr:
+            website = line.strip()
+            status = requests.get(website).status_code
+            status_dict[website] = "working" if status == 200 \
+                else "Not Working"
+    with open("website_status.csv", "w", newline="") as fw:
+        csv_writers = csv.writer(fw)
+        for key in status_dict.keys():
+            csv_writers.writerow([key, status_dict[key]])
+
+if __name__ == "__main__":
+    main()
