63961b07a2160c9c3ccb07e24a6a8299c9140b00
Mittesh Waghule <122700425+Mittesh-9@users.noreply.github.com>
2023-10-07 21:30:45 +0530

    Add files via upload

---
+++ python_lambda.py
@@ -0,0 +1,10 @@
+# A lambda function is a small anonymous function.
+
+# A lambda function can take any number of arguments, but can only have one expression.
+
+# Syntax
+# lamda arguments : expressions
+
+# Example:
+x = lambda a : a + 10
+print(x(10))
