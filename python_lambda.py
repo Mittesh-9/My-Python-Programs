7111fa6c877ffbbf45053c2ef9d002dd9587dc31
Mittesh Waghule <122700425+Mittesh-9@users.noreply.github.com>
2023-10-09 01:52:32 +0530

    Update python_lambda.py

--- python_lambda.py
+++ python_lambda.py
@@ -8,3 +8,11 @@
 # Example:
 x = lambda a : a + 10
 print(x(10))
+
+# Lambda functions can take any number of arguments:
+
+# Example
+# Multiply argument a with argument b and return the result:
+
+x = lambda a, b : a * b
+print(x(5, 5))
