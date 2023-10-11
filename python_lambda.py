e3bbe2d2e8dd35f6c3ef71edf4b98683614490b2
Mittesh Waghule <122700425+Mittesh-9@users.noreply.github.com>
2023-10-11 20:42:55 +0530

    Update python_lambda.py

--- python_lambda.py
+++ python_lambda.py
@@ -22,3 +22,17 @@ print(x(5, 5))
 
 x = lambda a, b, c : a + b + c
 print(x(1,2,5))
+
+# Why Use Lambda Functions?
+# The power of lambda is better shown when you use them as an anonymous function inside another function.
+
+# Say you have a function definition that takes one argument, and that argument will be multiplied with an unknown number:
+def myfunc(n):
+  return lambda a : a * n
+
+# Use that function definition to make a function that always doubles the number you send in:
+def myfunc(n):
+  return lambda a : a * n
+
+mydoubler = myfunc(2)
+print(mydoubler(11))
