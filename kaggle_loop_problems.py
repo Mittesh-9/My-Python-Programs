804b16ffd8007eb00e8eef835361fba06c7fcd4a
Mittesh Waghule <122700425+Mittesh-9@users.noreply.github.com>
2023-10-17 19:00:24 +0530

    Create kaggle_loop_problems.py

---
+++ kaggle_loop_problems.py
@@ -0,0 +1,50 @@
+# 1) Identify the bug in the below code. 
+
+def has_lucky_number(nums):
+    """Return whether the given list of numbers is lucky. A lucky list contains
+    at least one number divisible by 7.
+    """
+    for num in nums:
+        if num % 7 == 0:
+            return True
+        else:
+            return False
+
+Solution -->
+
+# Remember that return causes a function to exit immediately. So our original implementation always ran for just one iteration. We can only return False if we've looked at every element of the list (and confirmed that none of them are lucky). Though we can return early if the answer is True 
+
+def has_lucky_number(nums):
+    """Return whether the given list of numbers is lucky. A lucky list contains
+    at least one number divisible by 7.
+    """
+    for num in nums:
+        if num % 7 == 0:
+            return True
+    return False
+
+# 2) def elementwise_greater_than(L, thresh):
+    """Return a list with the same length as L, where the value at index i is 
+    True if L[i] is greater than thresh, and False otherwise.
+    
+    >>> elementwise_greater_than([1, 2, 3, 4], 2)
+    [False, False, True, True]
+    """
+
+Solution -->
+
+def elementwise_greater_than(L, thresh):
+    """Return a list with the same length as L, where the value at index i is 
+    True if L[i] is greater than thresh, and False otherwise.
+    
+    >>> elementwise_greater_than([1, 2, 3, 4], 2)
+    [False, False, True, True]
+    """
+    bool_list = []
+    for i in L:
+        if i > thresh  :
+            bool_list.append(True)
+        else:
+            bool_list.append(False)
+    return(bool_list)
+
