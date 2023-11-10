ebec29ffa003fbe0fff44f2ea364da97af0a6e36
Mittesh Waghule <122700425+Mittesh-9@users.noreply.github.com>
2023-11-10 13:41:13 +0530

    Update factorial_of_a_number_without_recursion.py

--- factorial_of_a_number_without_recursion.py
+++ factorial_of_a_number_without_recursion.py
@@ -16,3 +16,10 @@ while(n > 0):
     n = n - 1
 print("Factorial of the number is : ")
 print(fact)
+
+# Program Explanation
+# 1. User must enter a number.
+# 2. A factorial variable is initialized to 1.
+# 3. A while loop is used to multiply the number to the factorial variable and then the number is decremented each time.
+# 4. This continues till the value of the number is greater than 0.
+# 5. The factorial of the number is printed.
