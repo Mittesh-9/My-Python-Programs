3603cf31811ba8303203a53690290b2b03c45cda
Mittesh Waghule <122700425+Mittesh-9@users.noreply.github.com>
2023-11-16 23:47:29 +0530

    Create find_average_of_a_list.py

---
+++ find_average_of_a_list.py
@@ -0,0 +1,10 @@
+n = int(input("Enter the number of elements to be inserted : "))
+
+a = []
+
+for i in range (0, n):
+    element = int(input("Enter element : ")
+    a.append(element)
+avg = sum(a) / n
+
+print("Average of elements in the list", round (avg, 2))
