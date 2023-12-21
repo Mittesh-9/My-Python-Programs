077b74889ce44f70fe17687fac64dd6c48fbb73d
Mittesh Waghule <122700425+Mittesh-9@users.noreply.github.com>
2023-12-21 22:24:52 +0530

    Update convert_bytes_to_string.py

--- convert_bytes_to_string.py
+++ convert_bytes_to_string.py
@@ -3,3 +3,13 @@ print(type(string1))
 
 string2 = b'Welcome to myplace'  
 print(type(string2)) 
+
+
+
+byteData = b"Lets eat a \xf0\x9f\x8d\x95!"  
+  
+print(type(byteData)) 
+
+str1 = byteData.decode('UTF-8')  
+print(type(str1))  
+print(str1)
