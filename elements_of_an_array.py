fb533a0de3ea7895f10f0a8978696b593e12e240
Mittesh Waghule <122700425+Mittesh-9@users.noreply.github.com>
2023-12-23 22:47:33 +0530

    Create elements_of_an_array.py

---
+++ elements_of_an_array.py
@@ -0,0 +1,12 @@
+# STEP 1: Declare and initialize an array.
+# STEP 2: Loop through the array by incrementing the value of i.
+# STEP 3: Finally, print out each element of the array.
+
+#Initialize array     
+arr = [1, 2, 3, 4, 5]     
+     
+print("Elements of given array: ")    
+
+#Loop through the array by incrementing the value of i         
+for i in range(0, len(arr)):    
+    print(arr[i]) 
