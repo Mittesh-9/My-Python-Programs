14ac1ef1cbfe38b630478bd8cf48110d803a28c2
Mittesh Waghule <122700425+Mittesh-9@users.noreply.github.com>
2024-01-22 02:04:57 +0530

    Update hr_find_the_percentage_problem.py

--- hr_find_the_percentage_problem.py
+++ hr_find_the_percentage_problem.py
@@ -6,3 +6,16 @@ if __name__ == '__main__':
         scores = list(map(float, line))
         student_marks[name] = scores
     query_name = input()
+
+# Solution >>
+    
+    if query_name in student_marks:
+        marks_to_percentage = student_marks[query_name] 
+        percentage_marks = []
+        for i in marks_to_percentage:
+            percentage_marks.append(i)
+        
+        average = sum(percentage_marks) / len(percentage_marks)
+         
+        formatted_average = f'{float(average):.2f}'
+        print(formatted_average)
