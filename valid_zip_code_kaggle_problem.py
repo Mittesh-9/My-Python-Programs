99cba74e7703b39ce28d682380edbb3638bff820
Mittesh Waghule <122700425+Mittesh-9@users.noreply.github.com>
2024-03-10 00:16:55 +0530

    Create valid_zip_code_kaggle_problem.py

---
+++ valid_zip_code_kaggle_problem.py
@@ -0,0 +1,6 @@
+def is_valid_zip(zip_code):
+    """Returns whether the input string is a valid (5 digit) zip code """
+  
+    if zip_code.isdigit() and len(zip_code) == 5:
+        return True
+    return False
